4b5d031061a1b181aa01f01c9030deaa1609269a
Robin Tram <robin.tram@gmail.com>
2019-01-09 11:38:15 -0500

    Auth Part 1.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
+  has_secure_password
+  validates :username, uniqueness: { case_sensitive: false }
+
   has_many :game_users
   has_many :games, through: :game_users
 
--- db/schema.rb
+++ db/schema.rb
@@ -53,8 +53,10 @@ ActiveRecord::Schema.define(version: 2019_01_08_230348) do
   end
 
   create_table "users", force: :cascade do |t|
+    t.string "username"
     t.string "first_name"
     t.string "last_name"
+    t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
