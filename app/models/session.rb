ed0fd7cb1fad9dd7122eca7da4e8b5ed971bd9e1
Robin Tram <robin.tram@gmail.com>
2019-01-18 01:22:26 -0500

    Thursday Midnight Commit.

--- app/models/session.rb
+++ app/models/session.rb
@@ -1,3 +1,8 @@
 class Session < ApplicationRecord
+  validates :date, presence: true
+  validates :location, presence: true
+  validates :hours, presence: true
+  validates :amount, presence: true
+
   belongs_to :user
 end
